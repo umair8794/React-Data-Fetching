f658641a328bcb868033788daed61a20fb9a9e59
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-20 15:33:57 +0500

    Fetch Data with React: Calling API with fetch method

--- src/App.js
+++ src/App.js
@@ -4,20 +4,38 @@ import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
 export default class App extends Component {
-  
+
   constructor() {
     super();
-  } 
+    this.state = {
+        gifs: []
+    };
+  }
 
-  render() { 
+  componentDidMount() {
+      fetch("http://api.giphy.com/v1/gifs/trending?api_key=sKWma0Fr0gk5wa65HWRFYRJy4EzhW57A")
+        .then(response => response.json())
+        .then(responseData => {
+            this.setState({
+                gifs: responseData.data
+            });
+        })
+        .catch(error => {
+            console.log("Error fetching and parsing data", error);
+        })
+  }
+
+  render() {
+    console.log(this.state.gifs);
+
     return (
       <div>
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
-          </div>   
-        </div>    
+            <SearchForm />
+          </div>
+        </div>
         <div className="main-content">
           <GifList />
         </div>
